350d17cd2b97d03af2e06a53e7074be8a8e93335
Seth Markarian <seth.h.markarian@gmail.com>
2021-10-03 16:43:01 -0400

    Added respected parquets

---
+++ strideNCESCrosswalkPrototypePython/dataset_to_parqet.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df = pd.read_csv('crosswalk.csv')
+df.to_parquet('crosswalk.parquet')
+
+df = pd.read_csv('FieldOfStudyData1617_1718_PP.csv')
+df.to_parquet('FieldOfStudyData1617_1718_PP.parquet')
+
+df = pd.read_excel('CIP2020_SOC2018_Crosswalk.xlsx')
+df.to_parquet('CIP2020_SOC2018_Crosswalk.parquet')
+
+# Below returns an error for some reason
+# df = pd.read_excel('national_M2020_dl.xlsx')
+# df.to_parquet('national_M2020_dl.parquet')
