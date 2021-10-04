6cff849f8be6fa6ca896ae6cb1ed00b7b46ece15
Seth Markarian <seth.h.markarian@gmail.com>
2021-10-04 16:21:49 -0400

    Fixed national_M2020_dl spreadsheet and turned it into a parquet

--- strideNCESCrosswalkPrototypePython/dataset_to_parqet.py
+++ strideNCESCrosswalkPrototypePython/dataset_to_parqet.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
-df = pd.read_csv('crosswalk.csv')
-df.to_parquet('crosswalk.parquet')
+df1 = pd.read_csv('crosswalk.csv')
+df1.to_parquet('crosswalk.parquet')
 
-df = pd.read_csv('FieldOfStudyData1617_1718_PP.csv')
-df.to_parquet('FieldOfStudyData1617_1718_PP.parquet')
+df2 = pd.read_csv('FieldOfStudyData1617_1718_PP.csv')
+df2.to_parquet('FieldOfStudyData1617_1718_PP.parquet')
 
-df = pd.read_excel('CIP2020_SOC2018_Crosswalk.xlsx', sheet_name = 1)
-df.to_parquet('CIP2020_SOC2018_Crosswalk.parquet')
+df3 = pd.read_excel('CIP2020_SOC2018_Crosswalk.xlsx', sheet_name = 1)
+df3.to_parquet('CIP2020_SOC2018_Crosswalk.parquet')
 
 # Below returns an error for some reason
-# df = pd.read_excel('national_M2020_dl.xlsx', sheet_name = 0)
-# df.to_parquet('national_M2020_dl.parquet')
+df4 = pd.read_excel('national_M2020_dl.xlsx', sheet_name = 0)
+df4.to_parquet('national_M2020_dl.parquet')
