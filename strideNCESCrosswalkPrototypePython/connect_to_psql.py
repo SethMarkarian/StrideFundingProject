e2839de0eb0afeed022359ea0d08ae26fcb59a17
Seth Markarian <seth.h.markarian@gmail.com>
2021-10-17 16:31:32 -0400

    Update connect_to_psql.py

--- strideNCESCrosswalkPrototypePython/connect_to_psql.py
+++ strideNCESCrosswalkPrototypePython/connect_to_psql.py
@@ -10,7 +10,7 @@ def config(filename = 'database.ini', section = 'postgresql'):
 
     db = {}
     if parser.has_section(section):
-        params = parser.items(section
+        params = parser.items(section)
         for param in params:
             db[param[0]] = param[1]
         else:
