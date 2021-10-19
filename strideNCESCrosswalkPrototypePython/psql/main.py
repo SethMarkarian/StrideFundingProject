eb6ce6375987490c8aef6a4298c808f770a4ee39
Seth Markarian <seth.h.markarian@gmail.com>
2021-10-18 20:15:59 -0400

    still working on python and psql integration

---
+++ strideNCESCrosswalkPrototypePython/psql/main.py
@@ -0,0 +1,7 @@
+from src.data.db_conn import load_db_table
+from config.config import get_project_root
+# Project root
+PROJECT_ROOT = get_project_root()
+# Read database - PostgreSQL
+df = load_db_table(config_db = 'database.ini', query = 'SELECT * FROM bls2020 LIMIT 5')
+print(df)
