14c8c9868c1f50bfda2e4f4675bc49374285243d
Seth Markarian <seth.h.markarian@gmail.com>
2021-11-08 15:40:09 -0500

    Update DatabaseConnect.R

--- R/DatabaseConnect.R
+++ R/DatabaseConnect.R
@@ -1,11 +1,19 @@
-install.packages('RPostgreSQL')
+#Have to follow this to install drivers for psql
+#https://db.rstudio.com/best-practices/drivers/
+
+# Install the latest odbc release from CRAN:
+install.packages("odbc")
+
+# Or the the development version from GitHub:
+# install.packages(devtools)
+devtools::install_github("rstats-db/odbc")
 
 
 
-
-
-host = "139.147.9.145",
-database = "stride_db",
-user = "public_reader",
-password = "oogleBoss.23",
-port = 5432)
+con <- DBI::dbConnect(odbc::odbc(),
+                      Driver   = "odbc",
+                      Server   = "139.147.9.145",
+                      Database = "stride_db",
+                      UID      = "public_reader",
+                      PWD      = rstudioapi::askForPassword("public_reader password"),
+                      Port     = 5432)
