e3dbf5286e5cec1a6f245571025190d4627d6cb6
Seth Markarian <seth.h.markarian@gmail.com>
2021-11-15 14:37:33 -0500

    Updated Fuzzy Matching Implementation

--- strideNCESCrosswalkPrototypePython/socLookupFuzzySoundex.py
+++ strideNCESCrosswalkPrototypePython/socLookupFuzzySoundex.py
@@ -1,14 +1,10 @@
+# Must have this installed in psql
+# 'CREATE EXTENSION fuzzystrmatch;'
+
 from socket import socket
 import pandas as pd
 import sys
 from psql.main import getPSQLOutput
-
-# def SOCLookup(title, df):
-#     pd.set_option('display.max_rows', None)
-#     newdf = df[df['soc2018codetitle'].str.contains(title, case = False)]
-#     newdf = newdf.drop_duplicates()
-#     print(newdf.to_string(index = False))
-#     main()
 
 def importingAndSetup(job_name):
     SOCdf = getPSQLOutput('SELECT SOC2018CodeTitle, SOC2018Code FROM cip2020_soc2018 where soundex(SOC2018CodeTitle) = soundex(' + "'" + job_name + "'" + ')')
@@ -19,9 +15,10 @@ def main():
     jobTitle = input("Enter Job Title: ")
     if jobTitle == '':
         sys.exit()
-    df = importingAndSetup()
-    print(df)
-    # SOCLookup(jobTitle, df)
-
+    df = importingAndSetup(jobTitle)
+    df = df.drop_duplicates()
+    print(df.to_string(index = False))
+    main()
+s
 if __name__ == '__main__':
     main()
