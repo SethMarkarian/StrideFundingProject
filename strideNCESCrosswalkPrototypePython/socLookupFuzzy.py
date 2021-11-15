a16023ffd3c1332cf7b51820ed20b6e56fc167be
Seth Markarian <seth.h.markarian@gmail.com>
2021-11-14 20:07:01 -0500

    First attempt at fuzzymatching

---
+++ strideNCESCrosswalkPrototypePython/socLookupFuzzy.py
@@ -0,0 +1,27 @@
+from socket import socket
+import pandas as pd
+import sys
+from psql.main import getPSQLOutput
+
+# def SOCLookup(title, df):
+#     pd.set_option('display.max_rows', None)
+#     newdf = df[df['soc2018codetitle'].str.contains(title, case = False)]
+#     newdf = newdf.drop_duplicates()
+#     print(newdf.to_string(index = False))
+#     main()
+
+def importingAndSetup(job_name):
+    SOCdf = getPSQLOutput('SELECT SOC2018CodeTitle, SOC2018Code FROM cip2020_soc2018 where soundex(SOC2018CodeTitle) = soundex(' + "'" + job_name + "'" + ')')
+    SOCdf = SOCdf[['soc2018codetitle', 'soc2018code']].copy()
+    return SOCdf
+
+def main():
+    jobTitle = input("Enter Job Title: ")
+    if jobTitle == '':
+        sys.exit()
+    df = importingAndSetup()
+    print(df)
+    # SOCLookup(jobTitle, df)
+
+if __name__ == '__main__':
+    main()
